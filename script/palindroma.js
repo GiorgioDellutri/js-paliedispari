d17d5b56b7c8160765d5045d0e412593e1b210f7
GiorgioDellutri <giorgiodll93@gmail.com>
2022-11-29 17:16:00 +0100

    corrections

--- script/palindroma.js
+++ script/palindroma.js
@@ -2,37 +2,33 @@
 // Chiedere all’utente di inserire una parola:
 // Creare una funzione per capire se la parola inserita è palindroma
 
-let parolaUtente = prompt("Inserisci una parola");
-let array = [];
-
-console.log(parolaUtente);
-
-for ( let i = 0; i < parolaUtente.length; i++){ 
-    array[i] = parolaUtente.charAt(i);
-}   
-console.log(array);
-
-let arrayInverso = array.reverse();
-console.log(arrayInverso);
-
-for (let i = 0; i < array.length; i++){
-
-    if (array[i] == arrayInverso[i]){
-        console.log("La parola non e' palindroma");
-    } else{
-        console.log("La parola e' palindroma");
-    }
-    
-}
-
-
-
-
-
-
-
-
 // ############################# MEMO #############################
 /*Il metodo charAt nelle stringhe restituisce un carattere di una stringa.
 * La posizione del carattere è indicata nell’indice tra parantesi tonde.
 * Quindi ad esempio charAt(0) restituisce il primo carattere, mentre charAt(1) restituisce il secondo e così via */
+
+let parolaUtente = prompt("Inserisci una parola");
+
+// function palindroma(){
+    let array = [];
+    
+    console.log(parolaUtente);
+    
+    for ( let i = 0; i < parolaUtente.length; i++){ 
+        array[i] = parolaUtente.charAt(i);
+    }   
+    console.log(array);
+    
+    let arrayInverso = array.reverse();
+    console.log(arrayInverso);
+    
+    for (let i = 0; i < array.length; i++){
+    
+        if (array[i] != arrayInverso[arrayInverso.length -1 - i]){
+            console.log("La parola non è palindroma");
+        } else{
+            console.log("La parola è palindroma");
+        }
+    }
+    // return palindroma();
+// }
